217d6c860b11e66fea87cd005da5f3e140e71c17
Bakr AbuHassiba <96659482+BakrAbuHassiba@users.noreply.github.com>
2023-12-16 00:02:45 +0200

    Add files via upload

---
+++ The-Sleeping-Teaching-Assistant-main/src/ta/TeachingAssistant.java
@@ -0,0 +1,50 @@
+
+package ta;
+
+import java.util.concurrent.Semaphore;
+
+
+public class TeachingAssistant implements Runnable {
+
+    private final Mutexlock wakeup;
+    private final Semaphore chairs;
+    private final Semaphore available;
+    private final Thread t;
+    private final int numberofTA;
+    private final int numberofchairs;
+    private final int helpTime=5000;
+
+
+    public TeachingAssistant (Mutexlock wakeup, Semaphore chairs, Semaphore available, int numberofTA,int numberofchairs) {
+        t = Thread.currentThread();
+        this.wakeup = wakeup;
+        wakeup = new Mutexlock(numberofTA);
+        this.chairs = chairs;
+        this.available = available;
+        this.numberofTA = numberofTA;
+        this.numberofchairs=numberofchairs;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                wakeup.release();
+                t.sleep(helpTime);
+                
+                
+                if (chairs.availablePermits() != numberofchairs) {
+                    int releasedChairs = 0;
+                    do {
+                        t.sleep(helpTime);
+                        chairs.release();
+                        releasedChairs++;
+
+                    } while (releasedChairs != numberofchairs);
+                }
+            } catch (InterruptedException e) {
+                continue;
+            }
+        }
+    }
+}
